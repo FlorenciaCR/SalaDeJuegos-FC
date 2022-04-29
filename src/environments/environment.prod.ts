05816ae8594805cba3eba84b960743b76e8ddc29
FlorenciaCR <florencia.colodro@hotmail.com>
2022-04-28 23:43:35 -0300

    deploy heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD6eSMNCc0_VUXCFtzRCR8BWDzsgeGq9lc",
+    authDomain: "tp-saladejuegoslabiv-fc.firebaseapp.com",
+    projectId: "tp-saladejuegoslabiv-fc",
+    storageBucket: "tp-saladejuegoslabiv-fc.appspot.com",
+    messagingSenderId: "892135385115",
+    appId: "1:892135385115:web:8b96d35cdd716d6925b0be"
+    }
 };
